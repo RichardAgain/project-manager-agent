3f26734d2a3650419400cc5b04356574c3763e60
richardagain <ric.and.mail@gmail.com>
2025-06-13 11:53:34 -0400

    Implement Bigram Language Model with training and evaluation in PyTorch; add data preprocessing and batch generation functions; create Jupyter notebook for testing and exploration.

--- main.py
+++ main.py
@@ -3,15 +3,6 @@ import tiktoken
 def main():
     with open("input/quijote.txt", "r", encoding="utf-8") as f:
         raw_text = f.read()
-        
-    print("Total number of character:", len(raw_text))
-    
-    text = ("hola como estamos mi gente") 
-
-    tokenizer = tiktoken.get_encoding("gpt2")
-    integers = tokenizer.encode(text)
-
-    print(tokenizer.decode(integers))
 
 if __name__ == "__main__":
     main()
